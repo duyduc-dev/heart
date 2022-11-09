088e0113e9685e30c7ce99ad2b6dcd5236bd2e7a
duyduc2003 <dangduyducdangduyduc@gmail.com>
2022-11-10 00:01:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const btn = $('#btn');
+const link = $('.href');
+const input = $('input[name="name"]');
+const text = $('.text');
+
+const optionHeart = [...$$('.option-heart')];
+
+const getParams = (link = window.location.search) => new Proxy(new URLSearchParams(link), {
+	get: (searchParams, prop) => searchParams.get(prop),
+});
+
+const app = {
+  params: {
+    name: getParams().name
+  },
+
+  option: {
+    current: 'pink',
+    PINK: 'pink',
+    RED: 'red'
+  },
+  
+  btnClick : function () {
+    if (btn) {
+      btn.onclick = e => {
+        link.href = `/${this.option.current == this.option.PINK ? 'heart-pink' : 'heart'}.html?name=${input.value || 'Duy Duc handsome'}`
+        link.click();
+      }
+    }
+  },
+
+
+  renderHeartPage() {
+    if (text) {
+      text.innerHTML = this.params.name;
+    }
+  },
+
+  optionHeartClick() {
+    if (optionHeart) {
+      for (let o of optionHeart) {
+        
+        o.onclick = e => {
+          this.option.current = o.dataset.option;
+          console.log("🚀 ~ file: main.js ~ line 48 ~ optionHeartClick ~ this.option.current", this.option.current)
+          optionHeart.forEach(i => i.classList.remove('option-active'))
+          o.classList.add('option-active')
+        }
+      }
+    }
+  },
+  
+  init() {
+    this.btnClick();
+    this.renderHeartPage();
+    this.optionHeartClick()
+  } 
+}
+
+
+app.init();
